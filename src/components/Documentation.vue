d69924c73933ab7271c2b30c9228b4ac0e403049
Anders Gustafson <anders@nemonisimors.com>
2023-07-22 16:58:04 +0200

    Updated versions.

--- src/components/Documentation.vue
+++ src/components/Documentation.vue
@@ -145,6 +145,16 @@
     </code>
   </a>
 
+  <p>
+    The following shows Los Angeles and Sydney, with UTC as selected local time zone:
+  </p>
+
+  <a href="https://anders.nemonisimors.com/projects/meetingTime/?tzLocal=UTC&tz=America/Los_Angeles&tz=Australia/Sydney">
+    <code>
+        https://anders.nemonisimors.com/projects/meetingTime/?tzLocal=UTC&tz=America/Los_Angeles&tz=Australia/Sydney
+    </code>
+  </a>
+
 
   <h2>About</h2>
 
@@ -153,7 +163,7 @@
   </p>
 
   <p>
-    Ideas came from the upcoming <a href="https://tc39.es/proposal-temporal/docs/">Temporal</a> proposal and especially the section <a href="https://tc39.es/proposal-temporal/docs/cookbook.html#book-a-meeting-across-time-zones">Book a meeting across time zones</a>. Since <em>Temporal</em> was not released in July 2021, <a href="https://moment.github.io/luxon">Luxon</a> (currently version <var>3.2.1</var>) is used instead to handle the otherwise error-prone datetime calculations. When Temporal is released, the underlying code may be refactored.
+    Ideas came from the upcoming <a href="https://tc39.es/proposal-temporal/docs/">Temporal</a> proposal and especially the section <a href="https://tc39.es/proposal-temporal/docs/cookbook.html#book-a-meeting-across-time-zones">Book a meeting across time zones</a>. Since <em>Temporal</em> was not released in July 2021, <a href="https://moment.github.io/luxon">Luxon</a> (currently version <var>3.3.0</var>) is used instead to handle the otherwise error-prone datetime calculations. When Temporal is released, the underlying code may be refactored.
   </p>
 
   <p>
@@ -163,6 +173,9 @@
   <h3>Changelog</h3>
 
   <ul>
+    <li>
+      Version <var>1.2.2</var>, released 2023-07-22.
+    </li>
     <li>
       Version <var>1.2.1</var>, released 2023-01-24.
     </li>
--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
 export const appVersion = __APP_VERSION__;
 // export const appName = __APP_NAME__;
-export const releaseDate = "2023-01-24";
+export const releaseDate = "2023-07-22";
 
 const convenienceSlots = {
     red: [21, 22, 23, 0, 1, 2, 3, 4, 5, 6],
