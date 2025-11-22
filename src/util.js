0622025aa47e58db5a658ea5202e6c5830ff486f
Anders Gustafson <anders@nemonisimors.com>
2025-11-23 00:07:02 +0100

    Update dependencies.

--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
 export const appVersion = __APP_VERSION__;
 // export const appName = __APP_NAME__;
-export const releaseDate = "2025-08-20";
+export const releaseDate = "2025-11-23";
 
 const convenienceSlots = {
     red: [21, 22, 23, 0, 1, 2, 3, 4, 5, 6],
